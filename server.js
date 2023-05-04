91176131ad10197d0cf0fb9ba8804feda86f9251
yash kshatriya <yashkshatriya.67@gmail.com>
2023-05-04 19:46:41 +0530

    fifth commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app =require("./backend/app");
 
 const server =http.createServer(app);
 
-var PORT=process.env.PORT;
+var port=process.env.PORT;
 
-server.listen(PORT || 3000, ()=>{
-    console.log("check 3000");
+server.listen(port, ()=>{
+    console.log(port);
 });
